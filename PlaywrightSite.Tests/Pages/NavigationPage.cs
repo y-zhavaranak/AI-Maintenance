9ef247df4be279bb7d399e0f03ac7ba86b20231a
y.zhavaranak <y.zhavaranak@godeltech.com>
2026-04-01 03:05:56 +0200

    break test

--- PlaywrightSite.Tests/Pages/NavigationPage.cs
+++ PlaywrightSite.Tests/Pages/NavigationPage.cs
@@ -4,8 +4,8 @@ namespace PlaywrightSite.Tests.Pages;
 
 public class NavigationPage : BasePage
 {
-    private static readonly By DocsButton = By.XPath("//a[normalize-space()='Docs']");
-    private static readonly By ApiButton = By.XPath("//a[normalize-space()='API']");
+    private static readonly By DocsButton = By.XPath("//a[normalize-space()='Doc']");
+    private static readonly By ApiButton = By.XPath("//a[normalize-space()='api']");
     private static readonly By CommunityButton = By.XPath("//a[normalize-space()='Community']");
 
     public NavigationPage(IWebDriver driver) : base(driver)
@@ -20,7 +20,7 @@ public class NavigationPage : BasePage
 
     public bool IsDocsButtonDisplayed() => WaitForElementVisible(DocsButton).Displayed;
 
-    public bool IsApiButtonDisplayed() => WaitForElementVisible(ApiButton).Displayed;
+    public bool IsApiButtonDisplayed() => WaitForElementVisible(ApiButton).Selected;
 
-    public bool IsCommunityButtonDisplayed() => WaitForElementVisible(CommunityButton).Displayed;
+    public bool IsCommunityButtonDisplayed() => WaitForElementVisible(CommunityButton).Enabled;
 }
